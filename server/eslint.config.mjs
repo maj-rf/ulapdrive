aa358a0aa3659910d7b3460bad863602d78857e2
maj-rf <rmkfermin@gmail.com>
2024-11-25 21:45:52 +0800

    fix: update no-unused-vars rule

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -5,5 +5,16 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
 );
