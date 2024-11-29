1546bafbfa60a7c8eea5e5b593b4a45e4fd05c24
maj-rf <rmkfermin@gmail.com>
2024-11-29 12:06:19 +0800

    fix: add proxy and properly load env for API_URL

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,27 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = `${env.VITE_API_URL ?? 'your_api_url'}`;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: API_URL,
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/^\/api\/v1/, ''),
+        },
+      },
+    },
+  };
 });
