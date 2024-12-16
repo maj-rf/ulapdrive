a84378669a38c66fca44a9f405e9e17be772dee6
maj-rf <rmkfermin@gmail.com>
2024-12-16 22:04:34 +0800

    feat: create upload file middleware and make req.locals typesafe by including fileDetails type

--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -1,7 +1,12 @@
+import { UploadApiResponse } from 'cloudinary';
 import { PublicUser } from '../types';
 
 declare module 'express-serve-static-core' {
   interface Request {
     user?: PublicUser;
+  }
+
+  interface Locals {
+    fileDetails?: UploadApiResponse;
   }
 }
