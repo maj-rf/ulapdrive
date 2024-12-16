45f6326aa51cac6b527110413fe67199f8b160e0
maj-rf <rmkfermin@gmail.com>
2024-12-16 22:03:48 +0800

    feat: implement file upload configs and utils with multer

---
+++ server/src/utils/uploadUtil.ts
@@ -0,0 +1,34 @@
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+import { env } from '../config/config';
+
+/**
+ * When using multer.memoryStorage(), it stores the file
+ * into a Buffer object which is a way to handle binary data
+ * such as files.
+ */
+
+cloudinary.config({
+  cloud_name: env.CLOUD_NAME,
+  api_key: env.CLOUD_API_KEY,
+  api_secret: env.CLOUD_API_SECRET,
+});
+
+const storage = multer.memoryStorage();
+export const upload = multer({ storage });
+
+export const uploadToCloud = async (fileString: string) => {
+  const { uploader } = cloudinary;
+  const res = await uploader.upload(fileString);
+  return res;
+};
+
+export const deleteFromCloud = async (url: string) => {
+  const { uploader } = cloudinary;
+  const parsedUrl = new URL(url);
+  const pathComponents = parsedUrl.pathname.split('/');
+  const lastSegment = pathComponents.at(-1) as string;
+  const public_id = lastSegment.replace(/\.[^/.]+$/, '');
+  const res = await uploader.destroy(public_id);
+  return res;
+};
