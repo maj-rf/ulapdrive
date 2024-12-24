f389a370a4f7f994a2e7381e06e4c70aa46a5310
maj-rf <rmkfermin@gmail.com>
2024-12-24 22:45:21 +0800

    fix: handle session expiration and show toast on error

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { useMe } from '@/hooks/useMe';
 import { Navigate } from 'react-router';
 import { Loading } from './Loading';
+import { toast } from 'sonner';
 
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { data, isLoading } = useMe();
+  const { data, isLoading, error } = useMe();
   if (isLoading) return <Loading />;
+  // redirect to login if session is expired and show error message
+  if (error) {
+    toast.error(error.message);
+    return <Navigate to="/login" />;
+  }
   return data ? children : <Navigate to="/login" />;
 };
