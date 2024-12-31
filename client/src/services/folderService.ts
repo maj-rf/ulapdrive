47932df7cf140cf265a17a3335586adbea670db2
maj-rf <rmkfermin@gmail.com>
2024-12-31 12:20:40 +0800

    feat: implement create and delete folder service

--- client/src/services/folderService.ts
+++ client/src/services/folderService.ts
@@ -12,3 +12,34 @@ export const getRootFolders = async (): Promise<Folder[]> => {
   }
   return data;
 };
+
+export const createFolder = async (name: string): Promise<Folder[]> => {
+  const res = await fetch(`${API_URL}/folders`, {
+    mode: 'cors',
+    credentials: 'include',
+    method: 'POST',
+    body: JSON.stringify({
+      name,
+    }),
+    headers: { 'Content-type': 'application/json' },
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.error);
+  }
+  return data;
+};
+
+export const deleteFolder = async (folderId: string): Promise<Folder[]> => {
+  const res = await fetch(`${API_URL}/folders/${folderId}`, {
+    mode: 'cors',
+    credentials: 'include',
+    method: 'DELETE',
+    headers: { 'Content-type': 'application/json' },
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.error);
+  }
+  return data;
+};
