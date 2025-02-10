4e56ff36896ef3c40e7bc19d4e771c99591ebff2
maj-rf <rmkfermin@gmail.com>
2025-02-10 09:58:55 +0800

    feat: update default Homepage and remove Breadcrumb when on Homepage

--- client/src/components/Breadcrumb.tsx
+++ client/src/components/Breadcrumb.tsx
@@ -1,14 +1,16 @@
-import { useParams, NavLink } from 'react-router';
+import { useParams, NavLink, useLocation } from 'react-router';
 import { ChevronRight, House } from 'lucide-react';
 import { useSingleFolder } from '@/hooks/useFolder';
 export const Breadcrumb = () => {
   const params = useParams();
+  const u = useLocation();
   const { data } = useSingleFolder(params.folderId as string);
+  if (u.pathname === '/') return null;
 
   return (
     <div className="inline-flex items-center gap-1">
-      <NavLink to="/" className="inline-flex gap-1">
-        <House />
+      <NavLink to="/" className="inline-flex gap-1 items-center">
+        <House size={20} />
         <span>Home </span>
       </NavLink>
       {!data ? (
