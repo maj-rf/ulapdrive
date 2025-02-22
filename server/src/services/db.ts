4c44f2083be36023fd86a3079b499fa722283e56
maj-rf <rmkfermin@gmail.com>
2025-02-22 21:53:54 +0800

    fix: add test db case and add scripts for prod

--- server/src/services/db.ts
+++ server/src/services/db.ts
@@ -2,7 +2,18 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+export const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url:
+          process.env.NODE_ENV === 'test'
+            ? process.env.DATABASE_TEST_URL
+            : process.env.DATABASE_URL,
+      },
+    },
+  });
 
 // Prevent multiple instances of Prisma Client in development (hot reloading can create new instances)
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
